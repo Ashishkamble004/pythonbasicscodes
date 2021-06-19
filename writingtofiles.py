5a05f90bbd2461ea726c36b4d7e503f1d5b9caf8
Ashish Kamble <kambleashish004@gmail.com>
2021-06-19 16:50:11 +0530

    Writing to a File example

---
+++ writingtofiles.py
@@ -0,0 +1,4 @@
+employee_file = open("emp.txt", "a")
+employee_file.write("\nAngela = Accountant")
+employee_file.write("\nToby = Human Resources")
+employee_file.write("\nKelly = Customer Service")
