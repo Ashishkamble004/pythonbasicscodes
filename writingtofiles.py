db498c3452bf3734704b3cf4fc0df0a66f6cb479
Ashish Kamble <kambleashish004@gmail.com>
2021-06-19 16:51:38 +0530

    Updating Writing to a File example. Closing the file

--- writingtofiles.py
+++ writingtofiles.py
@@ -2,3 +2,5 @@ employee_file = open("emp.txt", "a")
 employee_file.write("\nAngela = Accountant")
 employee_file.write("\nToby = Human Resources")
 employee_file.write("\nKelly = Customer Service")
+
+employee_file.close()
