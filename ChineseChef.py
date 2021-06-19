792a88fee5a6f276e21435847c93765d105c452f
Ashish Kamble <kambleashish004@gmail.com>
2021-06-19 19:36:04 +0530

    Inheritence in Python

---
+++ ChineseChef.py
@@ -0,0 +1,10 @@
+from Chef import Chef
+
+from Chef import Chef
+
+class ChineseChef(Chef):
+    #The above line inherits Chef class into ChineseChef
+    def make_fried_rice(self):
+        print("The chef makes fried rice")   
+    def make_special_dish(self):
+        print("The chef makes orange chicken")
---
+++ inheritence.py
@@ -0,0 +1,11 @@
+#refer Chef.py & ChineseChef.py for this
+from ChineseChef import ChineseChef
+from Chef import Chef
+
+myChef = Chef()
+myChef.make_chicken()
+
+myChineseChef = ChineseChef()
+myChineseChef.make_fried_rice()
+
+myChineseChef.make_special_dish()
