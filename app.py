bfae018a6880405267d162f652040cdd88524828
Ashish Kamble <kambleashish004@gmail.com>
2021-06-19 18:38:08 +0530

    Objects and classes in Python

---
+++ app.py
@@ -0,0 +1,7 @@
+from student import student
+#from student.py file import student class
+
+student1 = student("Jim", "Business", "3.1", False)
+student2 = student("Pam", "Art", "2.5", True)
+print(student1.name)
+print(student2.gpa)
